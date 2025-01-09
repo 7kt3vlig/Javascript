0360b2f75c7fff5d782e2642bd426da277555e97
Peter <p3t3rcr3ations@gmail.com>
2025-01-09 17:09:58 +0000

    javascript

---
+++ counter.js
@@ -0,0 +1,15 @@
+let counter = 0; 
+
+function count() {
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+
+    if (counter % 10 === 0) {
+        alert(`Count is now ${counter}`);
+    }
+}
+
+// Ensure that the DOM is fully loaded before attaching the event
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').onclick = count;
+});
